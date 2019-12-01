e925daecaa53a84ad9f85d1c0ce900e02a86a975
Your Name <baugusthildebrand@gmail.com>
2019-12-01 10:30:10 -0500

    Change to lambda backend

--- jw-backend.py
+++ jw-backend.py
@@ -1,8 +1,5 @@
-from flask import Flask, jsonify, request
-from gevent.pywsgi import WSGIServer
+import json
 import random
-
-app = Flask(__name__)
 
 wrong_reasons = [
     "Definitely.",
@@ -20,26 +17,10 @@ wrong_reasons = [
     "Beeg Beeg",
     "Wrongo Bongo" ]
 
-# Add cors headers after the fact
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    if request.method == 'OPTIONS':
-        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
-        headers = request.headers.get('Access-Control-Request-Headers')
-        if headers:
-            response.headers['Access-Control-Allow-Headers'] = headers
-    return response
-app.after_request(add_cors_headers)
-
-@app.route('/wrong-reason')
-def wrong_reason():
+def lambda_handler(event, context):
     idx = random.randint(0,len(wrong_reasons)-1)
-    response = {
-        'reason': wrong_reasons[idx]
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'reason': wrong_reasons[idx]})
     }
-
-    return jsonify(response)
-
-if __name__ == '__main__':
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
